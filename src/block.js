14a5ee2bb542805c54551f8d6a8b1b0568892de1
huiminlim <e0036379@u.nus.edu>
2020-02-05 17:05:09 +0800

    Debug: Basic blockchain init

--- src/block.js
+++ src/block.js
@@ -1,35 +1,40 @@
 // Block chain = linked list, each item contain hash of previous
-const BrewChain = function() {
-  let chain = [];
-  let currentBlock = {};
-  let genesisBlock = {};
+class BrewChain {
+  chain = [];
+  currentBlock = {};
+  genesisBlock = {};
 
   // Create blockchain with the initial genesis block
-  function init() {
+  constructor() {
     // Genesis block: Begin the chain at element 0
     // Start of the linked list
-    genesisBlock = {
+    this.genesisBlock = {
       index: 0,
       timestamp: new Date().getTime(),
       data: "Our genesis data",
       previousHash: "-1" // No previous block
     };
-
-    genesisBlock.hash = createHash(genesisBlock);
-    chain.push(genesisBlock);
-    currentBlock = genesisBlock;
-    console.log(genesisBlock.hash);
   }
 
-  function createHash({ timestamp, data, index, previousHash }) {
-    return Crypto.createHash("SHA256")
+  init() {
+    this.genesisBlock.hash = this.createHash(this.genesisBlock);
+    this.chain.push(this.genesisBlock);
+    this.currentBlock = this.genesisBlock;
+    console.log(this.genesisBlock.hash);
+  }
+
+  createHash({ timestamp, data, index, previousHash }) {
+    const crypto = require("crypto");
+    return crypto
+      .createHash("SHA256")
       .update(timestamp + data + index + previousHash)
       .digest("hex");
   }
 
   // Each block = data, hash of previous block
   // Need timestamp, index, data, hash, previous block hash
-};
+}
 
+// Test driver code
 let myBrew = new BrewChain();
 myBrew.init();
